057f5b5bac68b80d1ff98e51db7622b9cc3c8092
Pedro Pauluci <pauluci125romao.pedro@hotmail.com>
2021-04-22 13:47:17 -0300

    Conexao com Mongo DB

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,7 +1,15 @@
 
 const express = require('express');
+const mongoose = require('mongoose')
+
 const routes = require('./routes');
+
 const app = express();
+
+mongoose.connect('mongodb+srv://pauluci:31m052001@app.mil7z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 app.use(express.json())
 app.use(routes)
--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const UserController = require('../controller/User')
 const routes = express.Router()
 
 //GET ==> Usado somenta para buscar informação (no max mandar alguma query) -- SELECT
@@ -10,24 +11,9 @@ routes.get('/', (req, res) =>{
     return res.json({mensagem: 'pagina principal'})
 });
 
-routes.get('/produtos', (req, res) =>{
-    return res.json({mensagem: 'Rota de todos os produtos'})
-});
-
-routes.post('/produtos', (req, res) =>{
-    var produto = req.body.produto;
-    return res.json({mensagem: 'Add produto ' + produto.nome})
-});
-
-routes.put('/produtos', (req, res) =>{
-    var id = req.query.id;
-    var produto = req.body.produto;
-    return res.json({mensagem: 'Atualizar o produto ' + id +' com os dados do post ' + produto.nome})
-});
-
-routes.delete('/produtos', (req, res) =>{
-    var id = req.query.id;
-    return res.json({mensagem: 'Deleta o produto ' + id})
-});
+routes.get('/user', UserController.index);
+routes.post('/user', UserController.store);
+routes.put('/user', UserController.update);
+routes.delete('/user', UserController.delete);
 
 module.exports= routes;
